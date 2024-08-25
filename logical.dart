88ed7f63fb2cc1597b7ef4ce40352c010dacf996
marumf43 <mariumf43@gmail.com>
2024-08-25 13:27:48 +0500

    first commit

---
+++ logical.dart
@@ -0,0 +1,6 @@
+void main (){
+  var a = true;
+  var b = true;
+  var c = a && b;
+  var d = c && !a;
+}
---
+++ main.dart
@@ -0,0 +1,8 @@
+void main (){
+  String email = "metachemist13@gmail.com";
+  String user = "metachemist";
+  String passcode = "marium_4703";
+  print(email);
+  print(passcode);
+  print((email == "metachemist13@gmail.com" || user == "chemist") && passcode == "marium_4703");
+}
